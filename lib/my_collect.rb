0eaab3fab6a584dfcdb163cdf00bb2bfef00720b
Joan' Araujo <wisdombthegame@gmail.com>
2017-12-05 20:38:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
+def my_collect(languages)
+  counter = 0
+  empty_array = []
+  while counter < languages.length
+  empty_array << yield(languages[counter])
+      counter += 1
+end
+empty_array
+end
 
+
+my_collect(%w[ruby python closure]) {|names| "I love to code with #{names}."}
